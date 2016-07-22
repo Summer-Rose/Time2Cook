72e6f91ecf17c292d80cde970613c0bdfb795013
Summer Brochtrup <sbrochtrup@gmail.com>
2016-07-22 13:18:06 -0700

    move static system ringtone uri to Constants

--- app/src/main/java/com/summerbrochtrup/time2cook/Constants.java
+++ app/src/main/java/com/summerbrochtrup/time2cook/Constants.java
@@ -5,4 +5,5 @@ package com.summerbrochtrup.time2cook;
  */
 public class Constants {
     public static final String EXTRA_KEY_TIMER = "timer";
+    public static final String URI_SYSTEM_RINGTONE = "content://settings/system/ringtone";
 }
