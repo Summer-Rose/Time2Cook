47fd2d0a03ab9551f4fea9331854211de609cbe5
Summer Brochtrup <sbrochtrup@gmail.com>
2016-07-21 20:46:33 -0700

    add TimerActivity. Gather Timer object from intent extra

---
+++ app/src/main/java/com/summerbrochtrup/time2cook/ui/TimerActivity.java
@@ -0,0 +1,41 @@
+package com.summerbrochtrup.time2cook.ui;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+import com.summerbrochtrup.time2cook.Constants;
+import com.summerbrochtrup.time2cook.R;
+import com.summerbrochtrup.time2cook.models.Timer;
+
+import org.parceler.Parcels;
+
+public class TimerActivity extends AppCompatActivity {
+    private  final String TAG = getClass().getSimpleName();
+    private Timer mTimer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        mTimer = Parcels.unwrap(getIntent().getParcelableExtra(Constants.EXTRA_KEY_TIMER));
+        Log.d(TAG, "Timer name: " + mTimer.getTimerName());
+    }
+
+}
