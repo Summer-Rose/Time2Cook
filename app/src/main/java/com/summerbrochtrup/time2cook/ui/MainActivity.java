75941dc8db5a8171561d4abfeaa444db95b5bced
Summer Brochtrup <sbrochtrup@gmail.com>
2016-07-21 17:46:05 -0700

    bind gridview and set adapter

--- app/src/main/java/com/summerbrochtrup/time2cook/ui/MainActivity.java
+++ app/src/main/java/com/summerbrochtrup/time2cook/ui/MainActivity.java
@@ -8,14 +8,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridView;
 
 import com.summerbrochtrup.time2cook.R;
+import com.summerbrochtrup.time2cook.adapters.TimerGridAdapter;
 import com.summerbrochtrup.time2cook.models.Timer;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<Timer> mTimers = new ArrayList<>();
+    private GridView mGridView;
+    private TimerGridAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mGridView = (GridView) findViewById(R.id.timersGrid);
 
         mTimers.add(new Timer(R.drawable.image, "Brown Rice", "#99D1B7", "#F79273"));
         mTimers.add(new Timer(R.drawable.image_two, "White Rice", "#B1654B", "#FDC08E"));
@@ -32,5 +37,8 @@ public class MainActivity extends AppCompatActivity {
         mTimers.add(new Timer(R.drawable.image_two, "White Rice", "#B1654B", "#FDC08E"));
         mTimers.add(new Timer(R.drawable.image, "Hard Boiled Egg", "#FDC08E", "#B1654B"));
         mTimers.add(new Timer(R.drawable.image_two, "Quinoa", "#F79273", "#99D1B7"));
+
+        mAdapter = new TimerGridAdapter(this, mTimers);
+        mGridView.setAdapter(mAdapter);
     }
 }
