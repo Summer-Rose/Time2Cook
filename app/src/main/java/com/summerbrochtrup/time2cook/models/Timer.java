ef2d974cfd09f272262adc87a3621bf44cbc60aa
Summer Brochtrup <sbrochtrup@gmail.com>
2016-07-21 17:17:39 -0700

    create Timer model with name, color, and image properties. Time property is hardcoded for testing.

---
+++ app/src/main/java/com/summerbrochtrup/time2cook/models/Timer.java
@@ -0,0 +1,43 @@
+package com.summerbrochtrup.time2cook.models;
+
+/**
+ * Created by summerbrochtrup on 7/21/16.
+ */
+public class Timer {
+    private String mTimerName;
+    private String mImageBackgroundColor;
+    private String mTextBackgroundColor;
+    private int mImage;
+    private int mTime;
+
+    public Timer() {
+    }
+
+    public Timer(int image, String timerName, String imageBackgroundColor, String textBackgroundColor) {
+        mImage = image;
+        mTimerName = timerName;
+        mImageBackgroundColor = imageBackgroundColor;
+        mTextBackgroundColor = textBackgroundColor;
+        mTime = 11000;
+    }
+
+    public String getTimerName() {
+        return mTimerName;
+    }
+
+    public String getImageBackgroundColor() {
+        return mImageBackgroundColor;
+    }
+
+    public String getTextBackgroundColor() {
+        return mTextBackgroundColor;
+    }
+
+    public int getImage() {
+        return mImage;
+    }
+
+    public int getTime() {
+        return mTime;
+    }
+}
