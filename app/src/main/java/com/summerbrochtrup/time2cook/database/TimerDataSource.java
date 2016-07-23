0301ce83e128ebdfc3795d879498225d6b7e0c61
Summer Brochtrup <sbrochtrup@gmail.com>
2016-07-23 10:07:05 -0700

    add basic SQLite database schema and read/write methods

---
+++ app/src/main/java/com/summerbrochtrup/time2cook/database/TimerDataSource.java
@@ -0,0 +1,97 @@
+package com.summerbrochtrup.time2cook.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+import android.util.Log;
+
+import com.summerbrochtrup.time2cook.models.Timer;
+
+import java.util.ArrayList;
+
+/**
+ * Created by summerbrochtrup on 7/22/16.
+ */
+public class TimerDataSource {
+    private Context mContext;
+    private TimerSQLiteHelper mTimerSQLiteHelper;
+
+    public TimerDataSource(Context context) {
+        mContext = context;
+        mTimerSQLiteHelper = new TimerSQLiteHelper(context);
+        SQLiteDatabase database = mTimerSQLiteHelper.getReadableDatabase();
+        database.close();
+    }
+
+    private SQLiteDatabase open() {
+        return mTimerSQLiteHelper.getWritableDatabase();
+    }
+
+    private void close(SQLiteDatabase database) {
+        database.close();
+    }
+
+    public void create(Timer timer) {
+        SQLiteDatabase database = open();
+        database.beginTransaction();
+        ContentValues timerValues = new ContentValues();
+        timerValues.put(TimerSQLiteHelper.COLUMN_TIMER_NAME, timer.getTimerName());
+        timerValues.put(TimerSQLiteHelper.COLUMN_IMAGE, timer.getImage());
+        timerValues.put(TimerSQLiteHelper.COLUMN_TIME, timer.getTime());
+        timerValues.put(TimerSQLiteHelper.COLUMN_BACKGROUND_COLOR, timer.getImageBackgroundColor());
+        timerValues.put(TimerSQLiteHelper.COLUMN_TEXT_COLOR, timer.getTextBackgroundColor());
+        long timerID = database.insert(TimerSQLiteHelper.TIMERS_TABLE, null, timerValues);
+
+        database.setTransactionSuccessful();
+        database.endTransaction();
+        close(database);
+    }
+
+    public ArrayList<Timer> readTimers() {
+        SQLiteDatabase database = open();
+        Cursor cursor = database.query(
+                TimerSQLiteHelper.TIMERS_TABLE,
+                new String [] {
+                        BaseColumns._ID,
+                        TimerSQLiteHelper.COLUMN_TIMER_NAME,
+                        TimerSQLiteHelper.COLUMN_IMAGE,
+                        TimerSQLiteHelper.COLUMN_TIME,
+                        TimerSQLiteHelper.COLUMN_BACKGROUND_COLOR,
+                        TimerSQLiteHelper.COLUMN_TEXT_COLOR },
+                null, //Selection
+                null, //selection args
+                null, //group by
+                null, //having
+                null); //order
+        ArrayList<Timer> timers = new ArrayList<>();
+        if (cursor.moveToFirst()) {
+            Log.d("cursor", "move to first");
+            do {
+                Log.d("cursor", "inside do");
+                Timer timer = new Timer(getIntFromColumnName(cursor, BaseColumns._ID),
+                        getStringFromColumnName(cursor, TimerSQLiteHelper.COLUMN_TIMER_NAME),
+                        getIntFromColumnName(cursor, TimerSQLiteHelper.COLUMN_IMAGE),
+                        getIntFromColumnName(cursor, TimerSQLiteHelper.COLUMN_TIME),
+                        getStringFromColumnName(cursor, TimerSQLiteHelper.COLUMN_BACKGROUND_COLOR),
+                        getStringFromColumnName(cursor, TimerSQLiteHelper.COLUMN_TEXT_COLOR));
+                timers.add(timer);
+            } while(cursor.moveToNext());
+        }
+        Log.d("cursor", "done");
+        cursor.close();
+        close(database);
+        return timers;
+    }
+
+    private int getIntFromColumnName(Cursor cursor, String columnName) {
+        int columnIndex = cursor.getColumnIndex(columnName);
+        return cursor.getInt(columnIndex);
+    }
+
+    private String getStringFromColumnName(Cursor cursor, String columnName) {
+        int columnIndex = cursor.getColumnIndex(columnName);
+        return cursor.getString(columnIndex);
+    }
+}
---
+++ app/src/main/java/com/summerbrochtrup/time2cook/database/TimerSQLiteHelper.java
@@ -0,0 +1,42 @@
+package com.summerbrochtrup.time2cook.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+/**
+ * Created by summerbrochtrup on 7/22/16.
+ */
+public class TimerSQLiteHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "timers.db";
+    private static final int DB_VERSION = 1;
+    public static final String TIMERS_TABLE = "TIMERS";
+    public static final String COLUMN_TIMER_NAME = "NAME";
+    public static final String COLUMN_IMAGE = "IMAGE";
+    public static final String COLUMN_BACKGROUND_COLOR = "BACKGROUND_COLOR";
+    public static final String COLUMN_TEXT_COLOR = "TEXT_COLOR";
+    public static final String COLUMN_TIME = "TIME";
+    private static String CREATE_TIMERS = "CREATE TABLE " + TIMERS_TABLE
+            + "(" + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COLUMN_TIMER_NAME + " TEXT, "
+            + COLUMN_IMAGE + " INTEGER, "
+            + COLUMN_TIME + " INTEGER, "
+            + COLUMN_BACKGROUND_COLOR + " TEXT, "
+            + COLUMN_TEXT_COLOR + " TEXT)";
+
+
+    public TimerSQLiteHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TIMERS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
