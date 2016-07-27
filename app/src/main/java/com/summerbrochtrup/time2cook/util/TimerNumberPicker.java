27f5b98531fb75cd582d9f030b9c5582c56fe85f
Summer Brochtrup <sbrochtrup@gmail.com>
2016-07-26 22:26:44 -0700

    create custom NumberPicker helper to set font size

--- app/src/main/java/com/summerbrochtrup/time2cook/util/TimerNumberPicker.java
+++ app/src/main/java/com/summerbrochtrup/time2cook/util/TimerNumberPicker.java
@@ -1,7 +1,43 @@
 package com.summerbrochtrup.time2cook.util;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.EditText;
+
+import com.summerbrochtrup.time2cook.R;
+
 /**
  * Created by summerbrochtrup on 7/26/16.
  */
-public class TimerNumberPicker {
+public class TimerNumberPicker extends android.widget.NumberPicker {
+
+    public TimerNumberPicker(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void addView(View child) {
+        super.addView(child);
+        updateView(child);
+    }
+
+    @Override
+    public void addView(View child, int index, android.view.ViewGroup.LayoutParams params) {
+        super.addView(child, index, params);
+        updateView(child);
+    }
+
+    @Override
+    public void addView(View child, android.view.ViewGroup.LayoutParams params) {
+        super.addView(child, params);
+        updateView(child);
+    }
+
+    private void updateView(View view) {
+        if(view instanceof EditText){
+            ((EditText) view).setTextSize(30);
+        }
+    }
 }
