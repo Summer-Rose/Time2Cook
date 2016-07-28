433e1544a3c0abcfed6330669e653a05d979b390
Summer Brochtrup <sbrochtrup@gmail.com>
2016-07-28 13:45:51 -0700

    update constructors to take context and timer variables. Select fragment depending on context class

---
+++ app/src/main/java/com/summerbrochtrup/time2cook/adapters/TimerInputPagerAdapter.java
@@ -0,0 +1,59 @@
+package com.summerbrochtrup.time2cook.adapters;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.util.Log;
+
+import com.summerbrochtrup.time2cook.models.Timer;
+import com.summerbrochtrup.time2cook.ui.AddTimerActivity;
+import com.summerbrochtrup.time2cook.ui.AddTimerStepOneFragment;
+import com.summerbrochtrup.time2cook.ui.AddTimerStepTwoFragment;
+import com.summerbrochtrup.time2cook.ui.EditTimerStepOneFragment;
+import com.summerbrochtrup.time2cook.ui.EditTimerStepTwoFragment;
+
+/**
+ * Created by summerbrochtrup on 7/28/16.
+ */
+public class TimerInputPagerAdapter extends FragmentStatePagerAdapter {
+    private static int NUM_ITEMS = 2;
+    private Context mContext;
+    private Timer mTimer;
+
+    public TimerInputPagerAdapter(FragmentManager fragmentManager, Context context) {
+        super(fragmentManager);
+        mContext = context;
+    }
+
+    public TimerInputPagerAdapter(FragmentManager fragmentManager, Context context, Timer timer) {
+        super(fragmentManager);
+        mContext = context;
+        mTimer = timer;
+    }
+
+    @Override
+    public int getCount() {
+        return NUM_ITEMS;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                if (mContext.getClass().getSimpleName().equals(AddTimerActivity.class.getSimpleName())) {
+                    return AddTimerStepOneFragment.newInstance();
+                } else {
+                    return EditTimerStepOneFragment.newInstance(mTimer);
+                }
+            case 1:
+                if (mContext.getClass().getSimpleName().equals(AddTimerActivity.class.getSimpleName())) {
+                    return AddTimerStepTwoFragment.newInstance();
+                } else {
+                    return EditTimerStepTwoFragment.newInstance(mTimer);
+                }
+            default:
+                return null;
+        }
+    }
+}
