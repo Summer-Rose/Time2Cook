77e777c6eb25109218db91a176db7384e0fe298b
Summer Brochtrup <sbrochtrup@gmail.com>
2016-07-28 11:03:26 -0700

    add 'plus' icon to add direction in AddTimerActivity

---
+++ app/src/main/java/com/summerbrochtrup/time2cook/adapters/AddTimerPagerAdapter.java
@@ -0,0 +1,7 @@
+package com.summerbrochtrup.time2cook.adapters;
+
+/**
+ * Created by summerbrochtrup on 7/28/16.
+ */
+public class AddTimerPagerAdapter {
+}
---
+++ app/src/main/java/com/summerbrochtrup/time2cook/ui/AddTimerStepOneFragment.java
@@ -0,0 +1,7 @@
+package com.summerbrochtrup.time2cook.ui;
+
+/**
+ * Created by summerbrochtrup on 7/28/16.
+ */
+public class AddTimerStepOneFragment {
+}
